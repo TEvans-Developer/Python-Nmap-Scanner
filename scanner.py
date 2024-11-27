06e1d4f19b0557fcbd02bee00df5ba88b8158447
TEvans-Developer <140648793+TEvans-Developer@users.noreply.github.com>
2024-11-27 15:09:05 -0500

    Update scanner.py
    Added comments to explain sections of code

--- scanner.py
+++ scanner.py
@@ -2,21 +2,25 @@
 
 import nmap 
 
+#Initialize the PortScanner object
 scanner = nmap.PortScanner()
 
 print("Welcome, this is a simple nmap automation tool")
 print("<--------------------------------------------------->")
 
+#Takes in user input a IP address and returns IP address and type
 ip_addr = input("Please enter the IP address you want to scan:")
 print("The Ip you entered is:", ip_addr)
 type(ip_addr)
 
+#Takes in user input as 1-3 and returns response
 resp = input(""" \n Please enter the type of scan you want to run
                   1)SYN ACK Scan
                   2)UDP Scan
                   3)Comprehensive Scan \n""")
 print("You have selected options:", resp)
 
+#Print scan results, check if TCP ports are open. Provides verbose responses
 if resp =='1':
     print("Nmap Version: ", scanner.nmap_version())
     scanner.scan(ip_addr, '1-1024', '-v -sS')
